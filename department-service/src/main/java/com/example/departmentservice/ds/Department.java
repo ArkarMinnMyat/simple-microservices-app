2df9cfc38c18c767b23d4a2792cea9be399c675e
HackerMgPyone <arkarminmyat121@gmail.com>
2024-01-29 10:10:56 +0630

    Initial commit

---
+++ department-service/src/main/java/com/example/departmentservice/ds/Department.java
@@ -0,0 +1,26 @@
+package com.example.departmentservice.ds;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@AllArgsConstructor
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+    private String name;
+    private String country;
+
+    public Department() {
+
+    }
+}
